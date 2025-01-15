cbee36ae54d28b832c60f338dd84d18539897b7d
hls <hilliassmirali@gmail.com>
2025-01-15 22:56:27 +0100

    update the code

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,9 @@
 
 
 def print_last_digit(number):
-    print("{}".format(number % 10))
+    print("{}".format(number % 10), end="")
     return number % 10
+
+
+a = print_last_digit(242452)
+print(f"{a}")
