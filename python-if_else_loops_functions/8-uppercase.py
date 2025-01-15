eb4e2f812627dbe8482112a825d5eb3b8f0b244b
hls <hilliassmirali@gmail.com>
2025-01-15 22:45:53 +0100

    Create uppercase function

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
