59992dd8da1020dabc95747f56d8a7f60dc19697
hls <hilliassmirali@gmail.com>
2025-01-15 10:52:53 +0100

    update 7-edges

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_3_letters = word[:4]
+first_3_letters = word[:3]
 last_2_letters = word[-2:]
 middle_word = word[1:-1]
 print(f"First 3 letters: {first_3_letters}")
