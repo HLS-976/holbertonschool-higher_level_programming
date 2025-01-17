78d632dd2e66c7f94ef5f4515743f47caa666333
hls <hilliassmirali@gmail.com>
2025-01-17 19:42:43 +0100

    Change the format of print

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import random
-
+import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("{} is zero".format(number))
+    print(number, "is zero")
 elif number > 0:
-    print("{} is positive".format(number))
+    print(number, "is positive")
 else:
-    print("{} is negative".format(number))
+    print(number, "is negative")
