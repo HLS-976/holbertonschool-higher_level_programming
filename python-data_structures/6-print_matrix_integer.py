c8aff02223b46f37777724e6a7c8661b10c42cd1
hls <hilliassmirali@gmail.com>
2025-01-20 14:26:27 +0100

    Update print_matrix_integr script and add indesirable file in github

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            if (i[-1] != j):
-                print("{}".format(j), end=" ")
-            else:
-                print("{}".format(j))
+    if (matrix == [[]]):
+        print()
+    else:
+        for i in matrix:
+            for j in i:
+                if (i[-1] != j):
+                    print("{}".format(j), end=" ")
+                else:
+                    print("{}".format(j))
