39d7081b9f69b46f8e71feae424ef14ec49bb273
hls <hilliassmirali@gmail.com>
2025-01-21 16:25:23 +0100

    Create multiply_by_2 script

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+
+    for element in a_dictionary:
+        new_dict[element] = a_dictionary[element] * 2
+    return new_dict
