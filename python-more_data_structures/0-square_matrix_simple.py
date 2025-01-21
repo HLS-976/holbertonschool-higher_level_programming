80cc08374ab47cf60ff245f3aa2c539ecb8b96c4
hls <hilliassmirali@gmail.com>
2025-01-21 10:49:45 +0100

    Create square_matrix_simple script

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,27 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix
-    for i in matrix:
-        new_matrix = i
-        for j in i:
-            j = j ** 2
-    return new_matrix
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-
-for i in matrix:
-    print(f"{i}")
-
-print()
-
-for i in new_matrix:
-    print(f"{i}")
+    return [[x ** 2 for x in row] for row in matrix]
