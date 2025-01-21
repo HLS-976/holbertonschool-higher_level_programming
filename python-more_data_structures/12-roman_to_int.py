046f33bc30164d5e19868fb914c993dc26b5ef3d
hls <hilliassmirali@gmail.com>
2025-01-21 18:08:14 +0100

    Created roman_to_int script

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    convert_number = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = i = 0
+
+    if (roman_string is None or not str):
+        return (0)
+    
+    while (i < len(roman_string)):
+        for j in convert_number:
+            if (j == roman_string[i]):
+                result += convert_number[j]
+        i = i + 1
+    return result
