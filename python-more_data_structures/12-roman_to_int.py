eae48cca7fcdacd75f01ee568c22d5a269369be4
hls <hilliassmirali@gmail.com>
2025-01-21 23:12:24 +0100

    Update the code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    convert_number = {
-        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
-        }
-    new_arr = []
-    result = 0
+    convert_number = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    integer_value = 0
+    prev_value = 0
 
     if (roman_string is None or type(roman_string) is not str):
         return (0)
-    else:
-        if ("IX" in roman_string):
-            roman_string = roman_string.replace("IX", "")
-            result += 9
-        if ("XC" in roman_string):
-            roman_string = roman_string.replace("XC", "")
-            result += 90
-        if ("CM" in roman_string):
-            roman_string = roman_string.replace("CM", "")
-            result += 900
-        for i in roman_string:
-            for j in convert_number:
-                if (j == i):
-                    result += convert_number[j]
-    return result
+    
+    for char in reversed(roman_string):
+        value = convert_number.get(char, 0)
+        if value < prev_value:
+            integer_value -= value
+        else:
+            integer_value += value
+            prev_value = value
+    return integer_value
