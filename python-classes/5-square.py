328921cd13f3273ed2c8b5742db08b056ce09bc9
hls <hilliassmirali@gmail.com>
2025-01-28 12:19:14 +0100

    5-square file created

---
+++ python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This module provides the square class
+This is an empty class
+"""
+
+
+class Square:
+    """
+    The Square is an empty class
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """
+        Creates an instance of square with size attribute
+        Checks if size is an integer else raise an exception
+        """
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        this method get the value of __size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        this method set a value in __size
+        """
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        This method returns the area square result
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        This method prints the visually area formed by '#' charater
+        """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(1, self.__size):
+                print('#', end="")
+            print('#')
