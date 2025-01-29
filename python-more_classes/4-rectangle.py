7d4a141f85056b5a657302f1489d7a20129c3034
hls <hilliassmirali@gmail.com>
2025-01-29 15:59:00 +0100

    Update the code

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -9,10 +9,12 @@ class Rectangle:
     """
     This class is a representation of a rectangle
     """
+    __width = None
+    __height = None
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -33,10 +35,10 @@ class Rectangle:
         """
         Sets value on width
         """
-        if not (isinstance(self.__width, int)):
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("height must be >= 0")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
@@ -44,9 +46,9 @@ class Rectangle:
         """
         Sets value on width
         """
-        if not (isinstance(self.__height, int)):
-            raise TypeError("width must be an integer")
-        if self.__height < 0:
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
