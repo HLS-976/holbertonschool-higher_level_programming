bacba563feb7d7e055978c5106939f896f39f5b5
hls <hilliassmirali@gmail.com>
2025-01-29 10:54:05 +0100

    Create 0-rectangle file

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module provides the Rectangle class
+This class represent a rectangle
+"""
+class Rectangle:
+    """
+    This class is a representation of a rectangle
+    This class is empty
+    """
+    pass
