88003c87a9a415615dea8196d8eb4f8126104070
hls <hilliassmirali@gmail.com>
2025-01-29 15:50:50 +0100

    Update the code

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -11,7 +11,6 @@ class Rectangle:
     """
     __width = None
     __height = None
-
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
@@ -35,10 +34,10 @@ class Rectangle:
         """
         Sets value on width
         """
-        if not (isinstance(self.__width, int)):
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("height must be >= 0")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
@@ -46,9 +45,9 @@ class Rectangle:
         """
         Sets value on width
         """
-        if not (isinstance(self.__height, int)):
-            raise TypeError("width must be an integer")
-        if self.__height < 0:
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
