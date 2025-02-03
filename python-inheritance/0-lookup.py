c73a7b5f9856e7d911b36cb76ba4b194cbacef69
hls <hilliassmirali@gmail.com>
2025-02-03 21:52:31 +0100

    Create The 0-lookup file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module provides a the lookup function
+that returns the list of attributes of an object
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the number of available
+    attribute and methods of an object
+    """
+    return(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory is to manipulate the inheritance of class in python
