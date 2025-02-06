8a2518a5d462ddc26a299aba3d36ff02ad6ff7a0
hls <hilliassmirali@gmail.com>
2025-02-06 20:24:41 +0100

    Create the task_02_verboselist.py file

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module provides classes"""
+
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, item=-1):
+        value = super().pop(item)
+        print("Popped [{}] from the list.".format(value))
