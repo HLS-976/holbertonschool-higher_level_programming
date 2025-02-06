05382b489618392f37df7861fb76c94316e8dc4a
hls <hilliassmirali@gmail.com>
2025-02-06 15:29:34 +0100

    Create The file task_00_abc

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module provides the abstract package
+"""
+from abc import ABC
+
+
+class Animal(ABC):
+    """
+    This abstract class represent animal concept
+    providing the abstract method sound
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """
+    This class represent a dog
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    This class represent a cat
+    """
+    def sound(self):
+        return "Meow"
