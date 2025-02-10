79e0594866c0a6bd3df8e8f6c814fb54e92dd6f2
hls <hilliassmirali@gmail.com>
2025-02-10 15:14:24 +0100

    Update the code

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,11 +2,10 @@
 """
 This module provides the function from_json_string
 """
-import json
 
 
 def from_json_string(my_str):
     """
     This function return a python object by string json
     """
-    return json.loads(my_str)
+    return (my_str.__dict__)
