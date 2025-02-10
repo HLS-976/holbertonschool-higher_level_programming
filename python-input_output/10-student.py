bd7641c7703da1a141e732911a63474e28792748
hls <hilliassmirali@gmail.com>
2025-02-10 16:26:27 +0100

    Create 10-student file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module provides a class
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function return the attribute
+        contained in a list
+        """
+        dictionnary = {}
+        if attrs is None:
+            return self.__dict__
+        resultat = {}
+        for attr in sorted(attrs):
+            if attr in self.__dict__:
+                resultat[attr] = self.__dict__[attr]
+        return resultat
