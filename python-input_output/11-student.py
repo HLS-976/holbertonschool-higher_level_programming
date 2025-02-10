b77580cc0fd0f7505d5acb3c3fa907dfec54d361
hls <hilliassmirali@gmail.com>
2025-02-10 18:49:12 +0100

    Create 11-student file

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module provides a class
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function return the attribute
+        contained in a list
+        """
+        if attrs is None:
+            return self.__dict__
+        resultat = {}
+        for attr in sorted(attrs):
+            if attr in self.__dict__:
+                resultat[attr] = self.__dict__[attr]
+        return resultat
+    
+    def reload_from_json(self, json):
+        """
+        This function reloads the attributes from json
+        """
+        for key in json:
+            if key in self.__dict__:
+                self[key] = json[key]
+
