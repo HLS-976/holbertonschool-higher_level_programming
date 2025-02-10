225aecab5d1ace8cb9e358cfb8e8c3cacb7d4532
hls <hilliassmirali@gmail.com>
2025-02-10 10:59:30 +0100

    my_first_file.txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,6 @@ This module provides the funciont write_file
 
 
 def write_file(filename="my_first_file.txt", text=""):
+    """ Function that write in a file """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
