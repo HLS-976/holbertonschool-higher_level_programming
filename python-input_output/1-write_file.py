17b3ad6eb5933b95a3cd20e09fcecf6a21419666
hls <hilliassmirali@gmail.com>
2025-02-10 10:21:47 +0100

    Create the write_file file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module provides the funciont write_file
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w+', encoding='utf-8') as f:
+        return f.write(text)
