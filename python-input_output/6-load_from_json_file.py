dbec5dfd437e3b966b632e4af9e04438cf6db54d
hls <hilliassmirali@gmail.com>
2025-02-10 13:26:00 +0100

    Create the file load_from_json_file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module provides the function load_from_json"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    this function deserialize an json string
+    """
+    with open(filename, 'r+', encoding='utf-8') as f:
+        return json.load(f)
