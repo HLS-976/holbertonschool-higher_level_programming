761cb49a33edf999bedca95171ccb03aeff82589
hls <hilliassmirali@gmail.com>
2025-02-17 16:36:26 +0100

    Add comments

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,6 +5,9 @@ import csv
 
 
 def fetch_and_print_posts():
+    """
+    This function fetch data and print it
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     status_code = response.status_code
     print(f"Status code: {status_code}")
@@ -15,18 +18,23 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
+    """
+    This function fecth data and save some information
+    on a list and save on csv format
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     status_code = response.status_code
     list_of_data = []
     if status_code == 200:
         json_data = response.json()
         for element in json_data:
-            list_of_data.append({'id': element['id'], 'title': element['title'], 'body': element['body']})
+            list_of_data.append(
+                {'id': element['id'],
+                 'title': element['title'],
+                 'body': element['body']
+                 })
     with open('posts.csv', 'w') as f:
         writer = csv.DictWriter(f, fieldnames=['id', 'title', 'body'])
         writer.writeheader()
         for elem in list_of_data:
             writer.writerow(elem)
-
-fetch_and_print_posts()
-fetch_and_save_posts()
