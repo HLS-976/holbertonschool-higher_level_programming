f224516083e08fc2821fedf574fe8e6c51a3f494
hls <hilliassmirali@gmail.com>
2025-03-04 14:45:11 +0100

    Refactor SQL script to rename average score output column for clarity

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- this script computes the score average of all records in the table second_table
-SELECT AVG(score) AS average_score FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
