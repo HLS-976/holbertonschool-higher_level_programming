880cbdfb917c7ef56a2699773356dcc362b1d676
hls <hilliassmirali@gmail.com>
2025-03-05 11:17:16 +0100

    Fix SQL script to correct syntax and add FLUSH PRIVILEGES command

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- This script creates the mysql server user
-CREATE USER IF not EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd',
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
