f311bfb3740f3ed33f7fd7cf6de154afed14f556
hls <hilliassmirali@gmail.com>
2025-03-05 13:01:24 +0100

    Fix SQL query to filter cities of California by state_id instead of name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- This script lists all the cities of California that can be found in the database
-SELECT id, name FROM cities WHERE state_id = 'California' ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id;
