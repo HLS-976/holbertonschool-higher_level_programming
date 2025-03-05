d9d41be641cc517313d22500a5d0f0fe8ea3b7c9
hls <hilliassmirali@gmail.com>
2025-03-05 13:53:45 +0100

    Fix SQL script to correct table name and ensure proper ordering of genre IDs

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- This scripts lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows JOIN tv_shows_genres ON tv_shows_genres.show_id = tv_shows.id  ORDER BY tv_shows.title AND tv_shows_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id  ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
