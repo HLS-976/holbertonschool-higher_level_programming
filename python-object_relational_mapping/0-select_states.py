5265500637af580e3d055b90a12215923590e88d
hls <hilliassmirali@gmail.com>
2025-03-06 16:22:54 +0100

    Change SQL query to use ASC for ordering states by id

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
