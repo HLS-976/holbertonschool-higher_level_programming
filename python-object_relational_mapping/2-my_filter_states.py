8f5636f5c6dc61c7e6783a4b4b0ca21d1a739039
hls <hilliassmirali@gmail.com>
2025-03-06 23:12:54 +0100

    Fix SQL query in 2-my_filter_states.py to remove unnecessary ORDER BY clause

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute(
         "SELECT * FROM {} WHERE \
-        name = '{}' ORDER BY id ASC".format(table, name_to_search)
+        name = '{}'".format(table, name_to_search)
     )
     result = cursor.fetchall()
     for row in result:
