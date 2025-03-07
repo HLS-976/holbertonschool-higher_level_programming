85caf2abfcbf8f249b870fadf723585de4759ae7
hls <hilliassmirali@gmail.com>
2025-03-07 14:52:11 +0100

    Refactor database connection parameters and improve SQL query with INNER JOIN for clarity

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,18 +9,22 @@ if __name__ == "__main__":
         host='localhost',
         port=3306,
         user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     state_name = sys.argv[4]
+
     query = "SELECT cities.name FROM cities \
-            JOIN states ON states.id = cities.state_id \
+            INNER JOIN states ON states.id = cities.state_id \
             WHERE states.name LIKE BINARY %s \
             ORDER BY cities.id ASC"
+
     cursor = db.cursor()
     cursor.execute(query, (state_name,))
     result = cursor.fetchall()
+
     print(", ".join(row[0] for row in result))
+
     cursor.close()
     db.close()
