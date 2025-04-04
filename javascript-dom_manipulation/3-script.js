efb5d37ee19cf28742dff7b8c159ecffcdc593b9
hls <hilliassmirali@gmail.com>
2025-04-04 11:46:14 +0200

    Add toggle functionality for header color on button click

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+document.querySelector('#toggle_header').addEventListener('click', function() {
+  const toggle_header = document.querySelector('header');
+  if (
+    toggle_header.classList.contains('red')) {
+    toggle_header.classList.remove('red');
+    toggle_header.classList.add('green');
+  }
+  else {
+    toggle_header.classList.remove('green');
+    toggle_header.classList.add('red');
+  }
+});
